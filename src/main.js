458716a409e56ce13a1a05a5a20bf682f7618f90
Subho <contact.subhoghosh@gmail.com>
2026-09-12 15:28:31 +0530

    Prepare version 0.1.2

--- src/main.js
+++ src/main.js
@@ -52,7 +52,7 @@ export const CashbookApplication = GObject.registerClass(
                     application_name: 'Cashbook',
                     application_icon: 'io.subho.Cashbook',
                     developer_name: 'Subho',
-                    version: '0.1.1',
+                    version: '0.1.2',
                     developers: [
                         'Subho'
                     ],
